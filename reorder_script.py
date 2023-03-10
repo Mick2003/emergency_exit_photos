bdb71fc6cb35f24452b655a1e5c56d19b0100d3a
Mick2003 <Mick2003>
2023-03-10 14:04:31 +0100

    .

--- reorder_script.py
+++ reorder_script.py
@@ -18,11 +18,12 @@ Author: Mick Veenman
 
 import os
 
-folder_path = "./images"  # Replace with the path to your folder
+folder_paths = ["./images/test", './images/train']  # Replace with the path to your folder
 # folder_path = "D:\\Projects\\Python\\test\\emergency_sign\\test"  # Replace with the path to your folder
 # folder_path = "D:\\Projects\\Python\\test\emergency_sign\\train"  # Replace with the path to your folder
 
-def reoder():
+
+def reoder(folder_path):
     files = os.listdir(folder_path)
 
     for i, file_name in enumerate(files, 1):
@@ -30,7 +31,7 @@ def reoder():
         new_path = os.path.join(folder_path, f"{i}.jpg")
         os.rename(old_path, new_path)
 
-def rename_all():
+def rename_all(folder_path):
     files = os.listdir(folder_path)
     exts = [".jpg", ".png", ".gif"]  # Replace with the file extensions you want to rename
 
@@ -41,5 +42,6 @@ def rename_all():
             os.rename(os.path.join(folder_path, file_name), os.path.join(folder_path, new_name))
 
 
-rename_all() # First rename all files
-reoder() # Reorder files
+for folder_path in folder_paths:
+    rename_all(folder_path)
+    reoder(folder_path)
